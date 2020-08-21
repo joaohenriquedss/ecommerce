c3e0e977acedf2f798ff4596af22ed80f7dc71cd
joaohenriquedss <jhdss8194@gmail.com>
2020-08-21 09:21:55 -0300

    controller produto

--- app/Controllers/Http/Admin/OrderController.js
+++ app/Controllers/Http/Admin/OrderController.js
@@ -4,6 +4,8 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Order = use('App/Models/Order')
+const Database = use('Database')
 /**
  * Resourceful controller for interacting with orders
  */
@@ -17,7 +19,20 @@ class OrderController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index ({ request, response, paginate }) {
+    const {status, id} = request.only(['status','id'])
+    const query = Order.query()
+    if(status && id){
+      query.where('status', status)
+      query.orWhere('id','LIKE',`%${id}%`)
+    }else if(status){
+      query.where('status',status)
+    }else if(id){
+      query.where('id','LIKE',`%${id}%`)
+    }
+
+    const orders = query.paginate(paginate.page, paginate.limit)
+    return response.send(orders)
   }
 
   /**
@@ -52,19 +67,9 @@ class OrderController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing order.
-   * GET orders/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+  async show ({ params : {id},response}) {
+    const order = await Order.findOrFail(id)
+    return response.send(order)
   }
 
   /**
@@ -86,7 +91,20 @@ class OrderController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params: {id }, request, response }) {
+    const order = await Order.findOrFail(id)
+    const trx = await Database.beginTransaction()
+    try {
+      await order.items().delete(trx)
+      await order.coupons().delete(trx)
+      await order.delete(trx)
+      return response.status(204).send()
+    }catch(error){
+      await trx.rollback()
+      return response.status(400).send({
+        message: 'Erro ao deletar este pedido!'
+      })
+    }
   }
 }
 

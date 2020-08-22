e8a9879b8db4dba4f28c37f1df3b9e8fd779fe6b
joaohenriquedss <jhdss8194@gmail.com>
2020-08-22 19:01:01 -0300

    aplicação de cupom de desconto

--- app/Services/Order/OrderService.js
+++ app/Services/Order/OrderService.js
@@ -1,4 +1,5 @@
 'use strict'
+const Database = use('Database')
 
 class OrderService {
   constructor(model, trx = false) {
@@ -6,7 +7,7 @@ class OrderService {
     this.trx = trx
   }
   async syncItemns(items) {
-    if(!Array.isArray(items)){
+    if (!Array.isArray(items)) {
       return false
     }
 
@@ -14,16 +15,16 @@ class OrderService {
     await this.model.items.createMany(items, this.trx)
 
   }
-  async updateItems(items){
+  async updateItems(items) {
     let currentItems = await this.model
-    .items()
-    .whereIn('id', items.map(item => item.id))
-    .fetch()
+      .items()
+      .whereIn('id', items.map(item => item.id))
+      .fetch()
     // delete os itens que o user não quer mais
     await this.model
-    .items()
-    .whereIn('id',items.map(item => item.id))
-    .delete(this.trx)
+      .items()
+      .whereIn('id', items.map(item => item.id))
+      .delete(this.trx)
 
     // Atualiza os valores e quantidades
     await Promise.all(currentItems.rows.map(async item => {
@@ -32,7 +33,71 @@ class OrderService {
     }))
 
   }
+  async canApplyDiscount(coupon) {
+    const couponProducts = await Database
+      .from('coupon_products')
+      .where('coupon_id', coupon.id)
+      .pluck('product_id')
+    // 'product_id => 1,product_id => 2'
 
+    const couponClients = await Database.from('coupon_user')
+      .where('coupon_id', coupon.id)
+      .pluck('user_id')
+    // verificar se o cupom não está associado a produtos e clientes especificos
+    if (Array.isArray(couponProducts) &&
+      couponProducts.length < 1 &&
+      Array.isArray(couponClients) &&
+      couponClients < 1) {
+      return true
+    }
+    let isAssociatedProducts, isAssociatedClients = false
+
+    if (Array.isArray(couponProducts) && couponProducts.length > 0) {
+      isAssociatedProducts = true
+    }
+
+    if (Array.isArray(couponClients) && couponClients.length > 0) {
+      isAssociatedClients = true
+    }
+
+    const productsMatch = await Database
+      .from('order_items')
+      .where('order_id', this.model.id)
+      .whereIn('product_id', couponProducts)
+      .pluck('product_id')
+
+    /**
+     * Uso 1 - O cupom esta associado a clientes e produtos
+     */
+    if (isAssociatedClients && isAssociatedProducts) {
+      const clienMatch = couponClients.find(
+        client => client === this.model
+      )
+      if (clienMatch && Array.isArray(productsMatch) && productsMatch.length > 0) {
+        return true
+      }
+    }
+/**
+ * caso 2 - coupon associado apenas a produto
+ */
+    if(isAssociatedProducts && Array.isArray(productsMatch) && productsMatch.length > 0){
+      return true
+    }
+    /**
+     * 3 - o cupom está associado a 1 ou mais clientes
+     */
+    if(isAssociatedClients && Array.isArray(couponClients) && couponClients.length > 0){
+      const match = couponClients.find(client => client == this.model.user_id)
+      if(match){
+        return true
+      }
+    }
+    /**
+     * caso nenhuma positiva
+     * então o cupom está associado a cliente ou produtos ou os dois
+     * porém nenhum dos produtos deste pedido está legivel ao desconto
+     */
+    return false
+  }
 }
-
 module.exports = OrderService

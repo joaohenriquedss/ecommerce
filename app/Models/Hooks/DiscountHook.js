52b2e1a115497aaa9bb77df9425267993d897381
joaohenriquedss <jhdss8194@gmail.com>
2020-08-25 19:46:19 -0300

    criando hook

--- app/Models/Hooks/DiscountHook.js
+++ app/Models/Hooks/DiscountHook.js
@@ -1,7 +1,12 @@
 'use strict'
+const Coupon = use('App/Models/Coupon')
+const Order = use('App/Models/Order')
 
 const DiscountHook = exports = module.exports = {}
 
-DiscountHook.method = async (modelInstance) => {
-
+DiscountHook.method = async model => {
+  var couponProducts, discountItems = []
+  model.discount = 0
+  const coupon = await Coupon.find(model.coupon_id)
+  const order = await Order.find(model.order_id)
 }

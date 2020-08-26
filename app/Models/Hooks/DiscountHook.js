0233adf70f0a6c4ac1211c7f5960b1cc6148a178
joaohenriquedss <jhdss8194@gmail.com>
2020-08-26 19:00:50 -0300

    hook disconto

--- app/Models/Hooks/DiscountHook.js
+++ app/Models/Hooks/DiscountHook.js
@@ -9,4 +9,41 @@ DiscountHook.method = async model => {
   model.discount = 0
   const coupon = await Coupon.find(model.coupon_id)
   const order = await Order.find(model.order_id)
+
+  switch (coupon.can_use_for) {
+    case 'product_client' || 'product':
+      couponProducts = await Database.from('coupon_product')
+        .where('coupon_id', model.coupon_id)
+        .pluck('product_id')
+
+      discountItems = await Database.from('order_items')
+        .where('order_id', model.order_id)
+        .whereIn('product_id', couponProducts)
+
+      if (coupon.type == 'percent') {
+        for (let orderItem of discountItems) {
+          model.discount += (orderItem.subtotal / 100) * coupon.discount
+        }
+      } else if (coupon.type == 'currency') {
+        for (let orderItem of discountItems) {
+          model.discount += coupon.discount * orderItem.quantity
+        }
+      } else {
+        for(let orderItem of discountItems){
+          model.discount += orderItem.subtotal
+        }
+      }
+      break
+    default:
+      // client ou por todos
+      if(coupon.type == 'percent'){
+        model.discount = (order.subtotal / 100) * coupon.discount
+      }else if (coupon.type == 'currency') {
+        model.discount = coupon.discount
+      } else {
+        model.discount = order.subtotal
+      }
+      break
+  }
+  return model
 }

3fa7ea626d14745433d9cbb6cc4c848931e99914
joaohenriquedss <jhdss8194@gmail.com>
2020-09-01 21:14:26 -0300

    middleware

--- start/routes/auth.js
+++ start/routes/auth.js
@@ -7,20 +7,20 @@ const Route = use('Route')
  * Auth Routs
  */
 
- /**
-  * Agrupar rotas
-  */
+/**
+ * Agrupar rotas
+ */
 
-  Route.group(() => {
-    Route.post('register', 'Auth/AuthController.register').as('auth.register')
-    Route.post('login', 'Auth/AuthController.login').as('auth.login')
-    Route.post('refresh','Auth/AuthController.refresh').as('auth.refresh')
-    Route.post('logout','Auth/AuthController.logout').as('auth.logout')
+Route.group(() => {
+  Route.post('register', 'Auth/AuthController.register').as('auth.register').middleware(['guest'])
+  Route.post('login', 'Auth/AuthController.login').as('auth.login').middleware(['guest'])
+  Route.post('refresh', 'Auth/AuthController.refresh').as('auth.refresh').middleware(['guest'])
+  Route.post('logout', 'Auth/AuthController.logout').as('auth.logout').middleware(['auth'])
 
-    // resetar senha rotas
+  // resetar senha rotas
 
-    Route.post('reset-password', 'Auth/AuthController.forgot').as('auth.forgot')
-    Route.get('reset-password', 'Auth/AuthController.remember').as('auth.remember')
-    Route.put('reset-passorwd','Auth/AuthController.reset').as('auth.reset')
+  Route.post('reset-password', 'Auth/AuthController.forgot').as('auth.forgot').middleware(['auth'])
+  Route.get('reset-password', 'Auth/AuthController.remember').as('auth.remember').middleware(['auth'])
+  Route.put('reset-passorwd', 'Auth/AuthController.reset').as('auth.reset').middleware(['auth'])
 
-  }).prefix('v1/auth')
+}).prefix('v1/auth')
